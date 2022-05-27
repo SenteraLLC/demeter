cdb78b62c4ddd349cfff1e599f4b15bce19d6031
JoelLarson <joel.larson@sentera.com>
2022-05-26 22:23:58 -0500

    Expose 'Details' to public API

--- demeter/__init__.py
+++ demeter/__init__.py
@@ -4,7 +4,10 @@ from .inputs import *
 from .function import *
 from .execution import *
 from .local import *
-from . import datasource
 
+from . import datasource
+from . import database
+
+from .database.details import Details
 from .transformation import Transformation
 

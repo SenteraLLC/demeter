e2a16413dfa82b95805fa584f27f8093eed48707
JoelLarson <joel.larson@sentera.com>
2022-06-13 16:04:20 -0500

    Merge 'make_http' and 'server' scripts

--- scripts/server.py
+++ scripts/server.py
@@ -1,10 +1,18 @@
 # Python 3 server example
 from typing import Callable
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
-from datetime import datetime
 
+import json
 import urllib
+from datetime import datetime
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+import psycopg2
+
+from demeter import getPgConnection
+from demeter import HTTPType, HTTPVerb
+from demeter.types.inputs import HTTPType, HTTPVerb
+from demeter.inputs import insertOrGetHTTPType
+
 
 hostName = "localhost"
 serverPort = 8080
@@ -15,8 +23,8 @@ dateToString : Callable[[datetime], str] = lambda d : d.strftime(dateFormat)
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self) -> None:
-        query = urllib.parse.urlparse(self.path).query # type: ignore
-        query = urllib.parse.unquote(query) # type: ignore
+        query = urllib.parse.urlparse(self.path).query
+        query = urllib.parse.unquote(query)
         query_components = dict(qc.split("=") for qc in query.split("&"))
         try:
             field_id = query_components["field_id"]
@@ -65,6 +73,49 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes(json.dumps(result), "utf-8"))
 
 
+def registerTypes() -> None:
+  http_verb_to_string : Callable[[HTTPVerb], str] = lambda v : v.name.lower()
+
+  http_verb_to_postgres = lambda v : psycopg2.extensions.AsIs("".join(["'", http_verb_to_string(v), "'"]))
+  psycopg2.extensions.register_adapter(HTTPVerb, http_verb_to_postgres)
+
+  connection = getPgConnection()
+  cursor = connection.cursor()
+  http_get = HTTPType(
+               type_name           = "http_uri_params_test_type",
+               verb                = HTTPVerb.GET,
+               uri                 = "http://localhost:8080",
+               uri_parameters      = ["field_id", "start_date", "end_date"],
+               request_body_schema = None,
+             )
+  http_get_id = insertOrGetHTTPType(cursor, http_get)
+  print("HTTP GET ID: ",http_get_id)
+
+
+  http_post = HTTPType(
+                type_name           = "http_request_body_test_type",
+                verb                = HTTPVerb.POST,
+                uri                 = "http://localhost:8080",
+                uri_parameters      = None,
+                request_body_schema = {"type": "object",
+                                       "properties": {
+                                         "field_id":   {"type": "number"},
+                                         "start_date": {"type": "string",
+                                         "format": "date"
+                                       },
+                                      "end_date":   {"type": "string",
+                                                     "format": "date"
+                                                    },
+                                      }
+                                     }
+            )
+  http_post_id = insertOrGetHTTPType(cursor, http_post)
+  print("HTTP POST ID: ",http_post_id)
+
+  connection.commit()
+
+
+
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
@@ -76,3 +127,6 @@ if __name__ == "__main__":
 
     webServer.server_close()
     print("Server stopped.")
+
+
+

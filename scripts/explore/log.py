6bae0dd0f71776e5ad9f4292a43db0a246147ef9
JoelLarson <joel.larson@sentera.com>
2022-10-24 10:40:53 -0500

    Add logger for explore script

---
+++ scripts/explore/log.py
@@ -0,0 +1,26 @@
+import os
+import logging
+
+from datetime import datetime
+
+did_setup = False
+
+def setupLogger(output_dir: str = "/tmp/") -> bool:
+  t = datetime.now().strftime("%m-%d %H:%M:%S")
+
+  #f = os.path.join(output_dir, f"demeter.explorer.{t}.log")
+  f = os.path.join(output_dir, f"demeter.explorer.log")
+
+  logging.basicConfig(filename=f,
+                      filemode='w',
+                      format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                      datefmt='%H:%M:%S',
+                      level=logging.DEBUG
+                     )
+  return True
+
+
+def getLogger(name : str = "demeter_explorer") -> logging.Logger:
+  if not did_setup:
+    setupLogger()
+  return logging.getLogger(name)

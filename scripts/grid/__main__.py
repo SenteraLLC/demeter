7ec24fad918af49dd66345795ea98dee1ca03c4d
JoelLarson <joel.larson@sentera.com>
2022-11-03 19:21:55 -0500

    Minor linting fixes

--- scripts/grid/__main__.py
+++ scripts/grid/__main__.py
@@ -1,26 +1,24 @@
+from typing import Iterator
+
 import argparse
 import asyncio
 import json
+from copy import deepcopy
 from datetime import datetime, timedelta, timezone
-from typing import Any, Dict, Iterator, Optional, Tuple
 
-from shapely import wkb  # type: ignore
+from shapely.geometry import Point  # type: ignore
+from shapely.geometry import Polygon as Poly
 
-from demeter.db import TableId, getConnection
-from demeter.grid import getNodePolygon, getRoot
 
-from . import main_loop
+from demeter.db import getConnection
+
 from .example import (
-    do_stop,
-    getLocalType,
     getPoints,
-    getStartingGeoms,
-    insertTree,
     pointsToBound,
 )
-from .search import findRootByPoint, getTree
-from .spatial_utils import getKey, getNodeKey
-from .valuer import Value, Valuer
+
+from .main import main
+
 
 DATE = "%Y-%m-%d"
 SHORT = DATE + "Z"
@@ -40,18 +38,9 @@ def parseTime(s: str) -> datetime:
     )
 
 
-import json
-
-from shapely.geometry import Point
-from shapely.geometry import Polygon as Poly  # type: ignore
-
-
 def parsePoly(s: str) -> Poly:
     coords = json.loads(s)
     return Poly(coords)
-
-
-from copy import deepcopy
 
 
 def yieldTimeRange(
@@ -67,88 +56,6 @@ def yieldTimeRange(
 
 from typing import List
 
-
-async def main(
-    cursor: Any,
-    stat: str,
-    date: datetime,
-    points: List[Point],
-) -> None:
-    s = stat
-    local_type_id = getLocalType(cursor, s)
-    existing_roots = findRootByPoint(cursor, points, local_type_id)
-
-    d = date
-    v = Valuer(d, s)
-    running = asyncio.create_task(v.request_loop())
-
-    node_id_lookup: Dict[str, TableId] = {}
-    seed_polys: List[Tuple[Poly, List[Poly], List[Point]]] = []
-
-    if not len(existing_roots):
-        root_id, root_node_id = getStartingGeoms(cursor, points, start_polygon, U, d, s)
-        k = getNodeKey(start_polygon, 0)
-        node_id_lookup[k] = root_node_id
-
-        root_value, _, __, root_points = await v.get_value(start_polygon, [], points)
-        stmt = "update node set value = %(v)s where node_id = %(n)s returning true as success"
-        args = {"v": root_value, "n": root_node_id}
-        cursor.execute(stmt, args)
-        results = cursor.fetchall()
-        success = results[0].success
-        if not success:
-            print("Failed to update root node value.")
-
-        if len(points) != len(root_points):
-            print(
-                "Some points did not fall in starting range: ",
-                len(points) - len(root_points),
-            )
-        if not len(root_points):
-            # TODO: Add this functionality
-            # print("No valid points of interest were provided. Performing exhaustive query. This may take awhile.")
-            raise Exception("No points of interest is not currently supported.")
-        seed_polys.append((start_polygon, [], root_points))
-
-    elif len(existing_roots) > 1:
-        raise Exception("Too many")
-
-    else:
-        # TODO: Where to store time delta
-        # import sys
-        root_id, points = existing_roots.popitem()
-        root = getRoot(cursor, root_id)
-        root_node_id = root.root_node_id
-        poly = getNodePolygon(cursor, root_node_id)
-
-        # TODO FIXME Fix time delta
-        existing = getTree(cursor, root_id, points, d, timedelta(days=1, minutes=-1))
-
-        # TODO: Handle key calc here or in 'toTree' sql?
-        # TODO: psycopg3 should let us skip directly to shapely::Polygon
-
-        # TODO: Could be smart about not loading every possible result
-        # v.load_existing({ getKey(wkb.loads(m["bounds"], hex=True)) : Value(m["value"]) for node_id, m in existing })
-        v.load_existing(m[1] for m in existing)
-
-        for node_id, n in existing.leaves.items():
-            # The leaf nodes may be looked up as parents in 'insertTree'
-            k = getNodeKey(wkb.loads(n["bounds"], hex=True), n["level"])
-            node_id_lookup[k] = node_id
-
-            # We need to store metadata for the nodes in the Valuer
-            # TODO: Do in SQL
-            my_ancestry = [getNodePolygon(cursor, node_id) for node_id in n["ancestry"]]
-            p = wkb.loads(n["bounds"], hex=True)
-            points = [wkb.loads(z, hex=True) for z in n["points"]]
-            if getNodeKey(p, n["level"]) not in v.existing_keys:
-                seed_polys.append((p, my_ancestry, points))
-
-    branches, leaves = await main_loop(start_polygon, do_stop, U, v, seed_polys)
-
-    # TODO: Child geoms need to be clipped to parent, then we can add constraint
-    insertTree(cursor, branches, leaves, node_id_lookup, root_id)
-    running.cancel()
 
 
 from dotenv import load_dotenv
@@ -220,8 +127,6 @@ if __name__ == "__main__":
     connection = getConnection()
     cursor = connection.cursor()
 
-    U = args.keep_unused
-
     # TODO: Remove the 'or getPoints' expr when there is a better means of testing
     points = args.points or getPoints(cursor)
 
@@ -230,6 +135,6 @@ if __name__ == "__main__":
     for d in yieldTimeRange(start, end, delta):
 
         for s in args.stats:
-            asyncio.run(main(cursor, s, d, points))
+            asyncio.run(main(cursor, s, d, points, start_polygon, args.keep_unused))
 
     connection.commit()

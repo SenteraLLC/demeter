d5eb2df1b93bec5b519c5fec17ccc3edcf94f1c8
tnigon <8249296+tnigon@users.noreply.github.com>
2022-12-09 18:32:40 -0600

    adding args for env keys to getConnection() so doesn't have to be fixed db connection

--- demeter/db/_postgres/connection.py
+++ demeter/db/_postgres/connection.py
@@ -21,15 +21,21 @@ def getEnv(
 
 def getConnection(
     cursor_type: Type[psycopg2.extensions.cursor] = psycopg2.extras.NamedTupleCursor,
+    host_key: str = "DEMETER_PG_HOST",
+    port_key: str = "DEMETER_PG_PORT",
+    pw_key: str = "DEMETER_PG_PASSWORD",
+    user_key: str = "DEMETER_PG_USER",
+    options_key: str = "DEMETER_PG_OPTIONS",
+    db_key: str = "DEMETER_PG_DATABASE",
 ) -> connection:
     register_adapter(set, lambda s: adapt(list(s)))  # type: ignore
 
-    host = getEnv("DEMETER_PG_HOST", "localhost")
-    port = getEnv("DEMETER_PG_PORT", 5432)
-    password = getEnv("DEMETER_PG_PASSWORD")
-    user = getEnv("DEMETER_PG_USER", "postgres")
-    options = getEnv("DEMETER_PG_OPTIONS", "")
-    database = getEnv("DEMETER_PG_DATABASE", "postgres")
+    host = getEnv(host_key, "localhost")
+    port = getEnv(port_key, 5432)
+    password = getEnv(pw_key)
+    user = getEnv(user_key, "postgres")
+    options = getEnv(options_key, "")
+    database = getEnv(db_key, "postgres")
     return psycopg2.connect(
         host=host,
         port=port,

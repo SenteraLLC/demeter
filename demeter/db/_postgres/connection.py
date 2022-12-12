a633540f72b712d5dd89026fda36ea5f614fd314
tnigon <8249296+tnigon@users.noreply.github.com>
2022-12-12 10:40:13 -0600

    allowing to pass parameters to connection.py

--- demeter/db/_postgres/connection.py
+++ demeter/db/_postgres/connection.py
@@ -8,6 +8,7 @@ import psycopg2.extras
 from psycopg2.extensions import adapt
 from psycopg2.extensions import connection
 from psycopg2.extensions import register_adapter
+from sqlalchemy.engine import Connection, create_engine, URL
 
 
 def getEnv(
@@ -25,23 +26,43 @@ def getConnection(
     port_key: str = "DEMETER_PG_PORT",
     pw_key: str = "DEMETER_PG_PASSWORD",
     user_key: str = "DEMETER_PG_USER",
+    db_key: str = "DEMETER_PG_DATABASE",
+    dialect: str = "postgresql+psycopg2",
+    schema_search_path: str = "test_demeter,public",
+) -> Connection:
+    connect_args = {
+        "options": "-csearch_path={}".format(schema_search_path),
+        "cursor_factory": cursor_type,
+    }
+    url_object = URL.create(
+        dialect,
+        host=getEnv(host_key, "localhost"),
+        port=getEnv(port_key, 5432),
+        username=getEnv(user_key, "postgres"),
+        password=getEnv(pw_key),
+        database=getEnv(db_key, "postgres"),
+    )
+    engine = create_engine(url_object, connect_args=connect_args)
+    return engine.connect()
+
+
+def getConnection_psycopg2(
+    cursor_type: Type[psycopg2.extensions.cursor] = psycopg2.extras.NamedTupleCursor,
+    host_key: str = "DEMETER_PG_HOST",
+    port_key: str = "DEMETER_PG_PORT",
+    pw_key: str = "DEMETER_PG_PASSWORD",
+    user_key: str = "DEMETER_PG_USER",
     options_key: str = "DEMETER_PG_OPTIONS",
     db_key: str = "DEMETER_PG_DATABASE",
 ) -> connection:
-    register_adapter(set, lambda s: adapt(list(s)))  # type: ignore
+    register_adapter(set, lambda s: adapt(list(s)))  # type: ignore - not sure what this does?
 
-    host = getEnv(host_key, "localhost")
-    port = getEnv(port_key, 5432)
-    password = getEnv(pw_key)
-    user = getEnv(user_key, "postgres")
-    options = getEnv(options_key, "")
-    database = getEnv(db_key, "postgres")
     return psycopg2.connect(
-        host=host,
-        port=port,
-        password=password,
-        options=options,
-        database=database,
-        user=user,
+        host=getEnv(host_key, "localhost"),
+        port=getEnv(port_key, 5432),
+        password=getEnv(pw_key),
+        options=getEnv(options_key, ""),
+        database=getEnv(db_key, "postgres"),
+        user=getEnv(user_key, "postgres"),
         cursor_factory=cursor_type,
     )

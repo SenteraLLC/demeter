3e5c9c27e4c640deea67f64ac98dba15ed81d49d
Marissa Kivi <marissakivi@Marissa-Kivi-MacBook-Pro.local>
2023-02-16 16:02:57 -0600

    rearranging dir a bit to more logical

---
+++ demeter/weather/_initialize.py
@@ -0,0 +1,77 @@
+import logging
+import subprocess
+from os import getenv
+from os.path import (
+    dirname,
+    join,
+    realpath,
+)
+from tempfile import NamedTemporaryFile
+
+from sqlalchemy.engine import Connection
+
+
+def initialize_weather_schema(conn: Connection, drop_existing: bool = False) -> bool:
+    """Initializes weather schema using database connection.
+
+    Returns True if schema was successfully initialized.
+
+    Args:
+        conn (sqlalchemy.engine.Connection): Connection to demeter database where schema should be created.
+        drop_existing (bool): Indicates whether or not an existing schema called `schema_name` should be dropped if exists.
+    """
+
+    # check if the given schema name already exists
+    stmt = """
+        select * from information_schema.schemata
+        where schema_name = %(schema_name)s
+    """
+    cursor = conn.connection.cursor()
+    cursor.execute(stmt, {"schema_name": "weather"})
+    results = cursor.fetchall()
+
+    # if the schema exists already, drop existing if `drop_existing` is True, else do nothing
+    if len(results) > 0:
+        logging.info("A schema of name 'weather' already exists in this database.")
+        if not drop_existing:
+            logging.info(
+                "No further action will be taken as no `--drop_existing` flag was passed."
+            )
+            logging.info(
+                "Add `--drop_existing` flag to command call if you would like to re-initialize the schema."
+            )
+            return False
+        else:
+            logging.info(
+                "`drop_existing` is True. The existing schema will be dropped and overwritten."
+            )
+            stmt = """DROP SCHEMA IF EXISTS weather CASCADE;"""
+            conn.execute(stmt)
+
+    # make temporary SQL file and overwrite schema name lines
+    file_dir = realpath(join(dirname(__file__)))
+
+    with NamedTemporaryFile() as tmp:
+        with open(join(file_dir, "schema_weather.sql"), "r") as schema_f:
+            schema_sql = schema_f.read()
+
+            # Add user passwords
+            schema_sql = schema_sql.replace(
+                "weather_user_password", getenv("weather_user_password")
+            )
+            schema_sql = schema_sql.replace(
+                "weather_ro_user_password", getenv("weather_ro_user_password")
+            )
+        print(schema_sql)
+        tmp.write(schema_sql.encode())  # Writes SQL script to a temp file
+        tmp.flush()
+        host = conn.engine.url.host
+        username = conn.engine.url.username
+        password = conn.engine.url.password
+        database = conn.engine.url.database
+        port = conn.engine.url.port
+        psql = f'PGPASSWORD={password} psql -h {host} -p {port} -U {username} -f "{tmp.name}" {database}'
+
+        subprocess.call(psql, shell=True)
+
+    return True
---
+++ scripts/initialize_weather_schema.py
@@ -0,0 +1,88 @@
+"""Initializes (or re-initializes) a Demeter weather schema instance for a given host and database environment.
+
+For help: python3 -m scripts.initialize_weather_schema --help
+
+This script requires that you have the appropriate superuser credentials for the database in your .env file. It also requires that you have passwords
+for the two weather schema users--weather_user and weather_ro_user-- in your .env file.
+
+Once you have those things ready to go, set up the schema by running: python3 -m scripts.initialize_weather_schema --database_env='DEV' --database_host='LOCAL'
+"""
+import argparse
+import logging
+import sys
+
+import click
+from dotenv import load_dotenv  # type: ignore
+from utils.logging.tqdm import logging_init
+
+from demeter.db import getConnection
+from demeter.weather._initialize import initialize_weather_schema
+
+if __name__ == "__main__":
+    c = load_dotenv()
+    logging_init()
+
+    parser = argparse.ArgumentParser(description="Initialize weather schema instance.")
+
+    parser.add_argument(
+        "--database_host",
+        type=str,
+        help="Host of database to query/change; can be 'AWS' or 'LOCAL'.",
+        default="LOCAL",
+    )
+
+    parser.add_argument(
+        "--database_env",
+        type=str,
+        help="Database instance to query/change; can be 'DEV' or 'PROD'.",
+        default="DEV",
+    )
+
+    parser.add_argument(
+        "--drop_existing",
+        action="store_true",
+        help="Should the schema be re-created if it exists?",
+        default=False,
+    )
+
+    # set up args
+    args = parser.parse_args()
+    database_host = args.database_host
+    database_env = args.database_env
+
+    if args.drop_existing:
+        drop_existing = True
+    else:
+        drop_existing = False
+
+    # ensure appropriate set-up
+    assert database_host in ["AWS", "LOCAL"], "`database_host` can be 'AWS' or 'LOCAL'"
+    assert database_env in ["DEV", "PROD"], "`database_env` can be 'DEV' or 'PROD'"
+
+    if database_host == "AWS":
+        if click.confirm(
+            "Are you sure you want to tunnel to AWS database?", default=False
+        ):
+            logging.info("Connecting to AWS database instance.")
+        else:
+            sys.exit()
+
+    if drop_existing:
+        if click.confirm(
+            "Are you sure you want to drop existing schema?", default=False
+        ):
+            pass
+        else:
+            logging.info("Continuing command with `drop_existing` set to False.")
+            drop_existing = False
+
+    ssh_env_name = f"SSH_DEMETER_{database_host}" if database_host == "AWS" else None
+    database_env_name = f"DEMETER-{database_env}_{database_host}_SUPER"
+
+    # set up database connection
+    logging.info("Connecting to database: %s", database_env_name)
+    conn = getConnection(env_name=database_env_name, ssh_env_name=ssh_env_name)
+
+    logging.info("Initializing weather schema instance")
+    _ = initialize_weather_schema(conn, drop_existing)
+    conn.close()

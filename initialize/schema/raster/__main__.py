5edb3166b5893adce6ae7b200139f64b560742f1
Marissa Kivi <marissakivi@Marissa-Kivi-MacBook-Pro.local>
2023-04-10 15:18:24 -0500

    updating raster main

--- initialize/schema/raster/__main__.py
+++ initialize/schema/raster/__main__.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 from utils.logging.tqdm import logging_init
 
 from ..._utils import confirm_user_choice
-from .._utils.workflow import main
+from .._utils.workflow import run_schema_initialization
 
 if __name__ == "__main__":
     c = load_dotenv()
@@ -59,7 +59,7 @@ if __name__ == "__main__":
         no_response="Continuing command with `drop_existing` set to False.",
     )
 
-    main(
+    run_schema_initialization(
         database_host=database_host,
         database_env=database_env,
         schema_name="raster",
